e0b085806f8db0ff7849ec7e22b8f610b532a48f
Nathan <nathan.wuiske@griffithuni.edu.au>
2019-04-09 16:09:23 +1000

    seeder for vouchers

---
+++ database/factories/VoucherFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Voucher::class, function (Faker $faker) {
+    return [
+        'name' => $faker->unique()->name,
+        'description' => $faker->text,
+        'photo' => $faker->imageUrl,
+        'expiry_date' => $faker->dateTime,
+        'tags_id' => $faker->numberBetween(1,10),
+        'categories_id' => $faker->numberBetween(1,10),
+        'facebook_link' => $faker->url,
+        'popular_flag' => $faker->numberBetween(0,1),
+    ];
+});
