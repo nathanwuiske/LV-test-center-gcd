746a3bade8ba183e1a43fbbb28f48d8b3b5e1322
Nathan <nathan.wuiske@griffithuni.edu.au>
2019-04-09 19:00:02 +1000

    fixed indentation

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 
 <body class="hold-transition sidebar-mini">
 <div class="wrapper">
-  <!-- Main Sidebar Container -->
+<!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <router-link to="/example" class="admin-sidebar-link">
       <img src="https://i.imgur.com/pi2jjiw.png" class="sidebar-image img-circle elevation-3">
@@ -21,12 +21,13 @@
                 <p style="color: white">Dashboard</p>
             </router-link>
           </li>
+
           <li class="nav-item">
-                <router-link to="/test" class="nav-link">
-                  <i class="nav-icon fas fa-envelope"></i>
-                  <p style="color: white">Vouchers Management</p>
-                </router-link>
-              </li>
+            <router-link to="/test" class="nav-link">
+                <i class="nav-icon fas fa-envelope"></i>
+                 <p style="color: white">Vouchers Management</p>
+            </router-link>
+         </li>
         </ul>
       </nav>
     </div>
@@ -37,29 +38,30 @@
 <div class="content-wrapper">
     <!-- Content Header-->
     <div class="content-header">
-      <div class="container-fluid">
-      <h1>Hello text that stays</h1>
-      </div>
+        <div class="container-fluid">
+            <h1>Hello text that stays</h1>
+        </div>
     </div>
 
     <!-- Main content -->
     <div class="content">
-      <div class="container-fluid">
+        <div class="container-fluid">
             <router-view></router-view>
-      </div>
+        </div>
     </div>
-  </div> <!-- end content-wrapper -->
+</div> 
+<!-- end content-wrapper -->
  
 
-  <!-- Footer Main -->
-  <footer class="main-footer">
+<!-- Footer Main -->
+<footer class="main-footer">
     <!-- Right side footer -->
     <div class="float-right d-none d-sm-inline">
-      Something on the right here maybe
+        Something on the right here maybe
     </div>
     <!-- Left side footer -->
     <strong>Copyright &copy; 2019 <a href="#">Some link</a></strong> All rights reserved.
-  </footer>
+</footer>
 </div>
 </body>
 
