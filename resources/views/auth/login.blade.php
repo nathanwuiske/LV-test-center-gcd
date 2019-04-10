2d9b77b413e18295d9a7aaeaec27fc6b207b02f5
Nathan <nathan.wuiske@griffithuni.edu.au>
2019-04-10 13:27:11 +1000

    login css updated

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,21 +2,27 @@
 
 @section('content')
 <div class="container">
+    
     <div class="row justify-content-center">
         <div class="col-md-8">
         @guest
+        <div class="heading">
+            <h1>GCD-Admin</h1>
+        </div>
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
+                <div class="card-header">Login</div>
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
-                        <div class="form-group row has-feedback">
+                        <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" placeholder="Enter your email" class="email-login-icon form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+                                <p class='control'>
+                                <input id="email" type="email" placeholder="Enter your email" class="email form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+                                <span class='input-icon'></span>
+                                </p>
                                 @if ($errors->has('email'))
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('email') }}</strong>
@@ -24,14 +30,14 @@
                                 @endif
                             </div>
                         </div>
-
+                        
                         <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
                             <div class="col-md-6">
-                                
-                                <input id="password" type="password" placeholder="Enter your account password" class="login form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-                                    
+                                <p class='control'>
+                                <input id="password" type="password" placeholder="Enter your account password" class="pass form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+                                <span class='input-icon-pass'></span>
+                                 </p>
                                 @if ($errors->has('password'))
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('password') }}</strong>
