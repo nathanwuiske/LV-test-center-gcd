0d6f41916393b0721b9cd57d5b7750c184319673
Nathan <nathan.wuiske@griffithuni.edu.au>
2019-05-15 12:05:01 +1000

    voucher favourites table

--- database/factories/VoucherFactory.php
+++ database/factories/VoucherFactory.php
@@ -5,11 +5,10 @@ use Carbon\Carbon;
 
 $factory->define(App\Voucher::class, function (Faker $faker) {
     return [
-        'name' => $faker->unique()->name,
+        'name' => $faker->unique()->country,
         'description' => $faker->text,
         'photo' => $faker->imageUrl,
         'expiry_date' => $faker->dateTimeBetween(Carbon::now(), '2021-00-00 00:00:00'),
-        'category' => $faker->firstName,
         'facebook_link' => $faker->url,
         'popular_flag' => $faker->numberBetween(0,1),
     ];
