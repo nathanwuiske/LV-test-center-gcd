a17f63f802af89411279076f36c83d85e596d2cf
Nathan <nathan.wuiske@griffithuni.edu.au>
2019-05-15 14:42:02 +1000

    backend authentication

--- routes/api.php
+++ routes/api.php
@@ -27,3 +27,18 @@ Route::apiResource('category', 'API\CategoryController');
 Route::apiResource('vouchercategory', 'API\VoucherCategoryController');
 
 Route::apiResource('archived', 'API\ArchivedController');
+
+
+
+Route::group([
+    'prefix' => 'auth'
+], function () {
+    Route::post('login', 'Auth\AuthController@login')->name('login');
+    Route::post('register', 'Auth\AuthController@register');
+    Route::group([
+      'middleware' => 'auth:api'
+    ], function() {
+        Route::get('logout', 'Auth\AuthController@logout');
+        Route::get('user', 'Auth\AuthController@user');
+    });
+});
