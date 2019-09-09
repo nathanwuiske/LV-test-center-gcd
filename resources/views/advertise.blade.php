8f7a04d0de0075b853a7eacdb0c956b719479a6c
Nathan <nathan.wuiske@griffithuni.edu.au>
2019-09-09 12:01:39 +1000

    advertise update

--- resources/views/advertise.blade.php
+++ resources/views/advertise.blade.php
@@ -7,7 +7,7 @@
          <div class="row">
             <!--  <div class="sec-title text-center mb50 wow rubberBand animated" data-wow-duration="1000ms"> -->
             <div class=" text-center mb50">
-               <h2 class="advert">Advertise with us</h2>
+               <h2 class="advert">We are responsible for millions of $$$ in turnover for Gold Coast Business</h2>
                <div class="devider"><i class="fas fa-arrow-down fa-lg"></i></div>
             </div>
            
@@ -15,7 +15,7 @@
                <div class="fact-item">
                   <i class="fas fa-bolt fa-3x"></i>
                   <h2>Fast</h2>
-                  <p>Ads live within 8-24hrs</p>
+                  <p>Ads live within 24-48hrs</p>
                </div>
             </div>
             <div class="col-md-4 col-sm-6 col-xs-12 text-center wow fadeInUp animated" data-wow-duration="500ms" data-wow-delay="600ms">
@@ -29,7 +29,7 @@
                <div class="fact-item">
                   <i class="fas fa-users fa-3x"></i>
                   <h2>Effective</h2>
-                  <p>Reach out to thousands of consumers</p>
+                  <p>Engage your business with thousands of customers</p>
                </div>
             </div>
          </div>
@@ -41,20 +41,83 @@
 <!-- Start Package 1 section -->
 <section id="features" class="features">
    <div class="container">
+         <p class="fontSize17" style="color:black"> (ABP 117) is an Australian Business Projects 10 year project study of Gold Coast community, 
+               consumer and business. This is not a project about advertising or discount companies, 
+               <strong>this is a project about community, consumer and business engagement.</strong> It is highly successful; 
+               it is free for the community and IT SUPPORTS LOCAL BUSINESSES.
+            </p>
+            <div style="background-color:#1B4151; padding:10px 10px 5px 15px; margin-bottom:15px;">
+               <p class="fontSize17" style="color:white">This project has been responsible
+                  for millions of $$$ in turnover for Gold Coast Business. It is direct contact with the largest 
+                  customer and consumer networks on the Gold Coast and it powerfully promotes Gold Coast business.
+               </p>
+            </div>
+            <p class="fontSize17" style="color:black">
+               This project savings businesses thousands of dollars on advertising <strong>and delivers</strong>
+               YEAR ROUND CONSUMER RESULTS.
+            </p>
+            <div style="background-color:#1B4151; padding:10px 10px 10px 15px;margin-top:15px;margin-bottom:40px;">
+               <p class="fontSize17" style="color:white;">The following medias service a Gold Coast <strong>consumer network of over 500,000</strong> Locals, Residents, Families, Seniors, AND Visitors to the Coast.</p>
+               <div class="main-table">
+                  <div  style="display: table-cell; ">
+                     <li class="advert-list-item">Gold Coast Printed Brochures (Letterbox Deliveries)</li>
+                     <li class="advert-list-item">Gold Coast Digital Brochures</li>
+                     <li class="advert-list-item">GoldCoastDiscounts.com.au</li>
+                     <li class="advert-list-item">FreeMealDeals.com.au</li>
+                     <li class="advert-list-item">Facebook / GoldCoastDiscounts</li>
+                     <li class="advert-list-item">GCD Android / Google App</li>
+                     <li class="advert-list-item">GCD IOS / Apple App</li>
+                  </div>
+                  <div class="advert-clarif">
+                     <p class="fontSize17 text-center" style="color:white">
+                        Don’t be mistaken. This is not a project about advertising and discounts... 
+                        you’re already offering a VALUE every day just to attract customers. This is a project of 
+                        study and development of multiple and countless business platforms that have been engineered
+                        for community, consumer and business engagement.<br><br> Can you engage your business with 100’s of
+                        thousands of consumers more effectively for better pricing and YEAR ROUND business results? 
+                        <br><br>It’s not possible!
+                     </p>
+                  </div>
+               </div>
+
+               <div class="main-table-phone">
+                        <li class="advert-list-item">Gold Coast Printed Brochures (Letterbox Deliveries)</li>
+                        <li class="advert-list-item">Gold Coast Digital Brochures</li>
+                        <li class="advert-list-item">GoldCoastDiscounts.com.au</li>
+                        <li class="advert-list-item">FreeMealDeals.com.au</li>
+                        <li class="advert-list-item">Facebook / GoldCoastDiscounts</li>
+                        <li class="advert-list-item">GCD Android / Google App</li>
+                        <li class="advert-list-item">GCD IOS / Apple App</li>
+                     <div style="background-color:black; padding: 10px 10px 10px 10px;margin-top:30px;">
+                        <p class="fontSize17 text-center" style="color:white">
+                           Don’t be mistaken. This is not a project about advertising and discounts... 
+                           you’re already offering a VALUE every day just to attract customers. This is a project of 
+                           study and development of multiple and countless business platforms that have been engineered
+                           for community, consumer and business engagement.<br><br> Can you engage your business with 100’s of
+                           thousands of consumers more effectively for better pricing and YEAR ROUND business results? 
+                           <br><br>It’s not possible!
+                        </p>
+                     </div>
+                  </div>
+            </div>
+          
       <div class="row">
          <h2 class="text-center mb10">Package 1</h2>
-         <p class="text-center fontSize17 mb20">Promote your business with 2 Ads for 12 Months advertising</p>
+         <p class="text-center fontSize17 mb20">12 months Gold Coast advertising, affordable for ALL local businesses.
+          <span class="advert-red"> Free artwork and formating into all above medias</span> ($150 per ad) save $450!
+         <br> If you can't afford or see this value in this package, you really need to question your qualifications!
+         </p>
          <p class="text-center fontSize17 mb20">This includes</p>
 
          <!-- Start info item -->
          <div class="col-md-3 wow fadeInLeft" data-wow-duration="500ms">
             <div class="service-item">
                <div class="service-icon">
-                  <i class="fas fa-palette fa-2x"></i>
+                  <i class="fas fa-ad fa-2x"></i>
                </div>
-               <div class="service-desc">
-                  <h3>FREE Artwork</h3>
-                  <p>Bonus free artwork on (x2) advertisements valued at <strong>$150</strong></p>
+               <div class="service-desc"> 
+                  <h3><span class="advert-red">x(3)</span> Advertisements</h3>
+                  <p>12 Months of powerful advertising with year round customer engagement.</p>
                </div>
             </div>
          </div>
@@ -64,11 +127,11 @@
          <div class="col-md-3 wow fadeInUp" data-wow-duration="500ms" data-wow-delay="300ms">
             <div class="service-item">
                <div class="service-icon">
-                  <i class="fas fa-plus-square fa-2x"></i>
+                  <i class="fas fa-newspaper fa-2x"></i>
                </div>
                <div class="service-desc">
-                  <h3>Buy one get 1 FREE</h3>
-                  <p>Get an additional digital media advertisement free of charge.</p>
+                  <h3><span class="advert-red">50,000</span> Printed Ads</h3>
+                  <p>Printed and distributed into Gold Coast letterboxes and homes for only 1 cent each.</p>
                </div>
             </div>
          </div>
@@ -81,8 +144,8 @@
                   <i class="fas fa-mobile-alt fa-2x"></i>
                </div>
                <div class="service-desc">
-                  <h3>Online redemption app</h3>
-                  <p>Your ads will be displayed on our new application for Android and iOS. </p>
+                  <h3><span class="advert-red">x(2)</span> Digital Ads</h3>
+                  <p>Advertising your business directly into Gold Coast customers mobiles phones.</p>
                </div>
             </div>
          </div>
@@ -95,8 +158,8 @@
                   <i class="fab fa-facebook-square fa-2x"></i>
                </div>
                <div class="service-desc">
-                  <h3>FREE Social media promotion &nbsp;</h3>
-                  <p>Your ads will be promoted on our Facebook page.</p>
+                  <h3><span class="advert-red">x(7)</span> Digital Medias &nbsp;</h3>
+                  <p>Your ads promoted on ALL our Medias & Apps GC consumers like, love & follow.</p>
                </div>
             </div>
          </div>
@@ -111,19 +174,19 @@
    <div class="container">
       <div class="row">
          <h2 class="text-center mb10">Package 2</h2>
-         <p class="text-center fontSize17 mb20">Promote your business to the MAX with 4 Powerful Ads for 12 Months</p>
+         <p class="text-center fontSize17 mb20"><span class="advert-red">(4x) the value of package 1.</span>
+         Promote your business to the MAX with 4 Powerful Ads and 1/4 Million (250,000) Brochure Ads.</p>
          <p class="text-center fontSize17 mb20">This includes</p>
 
          <!-- Start info item -->
          <div class="col-md-3 wow fadeInUp" data-wow-duration="500ms">
             <div class="service-item">
                <div class="service-icon">
-                  <i class="fas fa-newspaper fa-2x"></i>
+                  <i class="fas fa-ad fa-2x"></i>
                </div>
                <div class="service-desc">
-                  <h3>1/4 Million Brochure advertisements</h3>
-                  <p>Your ad will be printed on our brochures and distributed into the Gold Coast community via our
-                     popular Shopping Centre Stands and Gold Coast Letterboxes. </p>
+                  <h3><span class="advert-red">x(4)</span> Advertisements</h3>
+                  <p>12 Months of powerful advertising with year round customer engagement. </p>
                </div>
             </div>
          </div>
@@ -133,11 +196,11 @@
          <div class="col-md-3 wow fadeInLeft" data-wow-duration="500ms" data-wow-delay="500ms">
             <div class="service-item">
                <div class="service-icon">
-                  <i class="fas fa-plus-square fa-2x"></i>
+                  <i class="fas fa-newspaper fa-2x"></i>
                </div>
                <div class="service-desc">
-                  <h3>Buy one get 3 FREE</h3>
-                  <p>Get an additional 3 digital media advertisements.</p>
+                  <h3><span class="advert-red">1/4 Million</span> Printed Ads</h3>
+                  <p>Printed and ditributed into Gold Coast letterboxes and homes for LESS than 1/4 of a cent each.</p>
                </div>
             </div>
          </div>
@@ -150,8 +213,8 @@
                   <i class="fas fa-mobile-alt fa-2x"></i>
                </div>
                <div class="service-desc">
-                  <h3>Online redemption app</h3>
-                  <p>Your advertisements will be displayed on our new application for Android and iOS. </p>
+                  <h3><span class="advert-red">x(3)</span> Digital Ads</h3>
+                  <p>Advertising your business directly into Gold Coast customers mobile phones.</p>
                </div>
             </div>
          </div>
@@ -164,52 +227,47 @@
                   <i class="fab fa-facebook-square fa-2x"></i>
                </div>
                <div class="service-desc">
-                  <h3>FREE Social media promotion &nbsp;</h3>
-                  <p>Your ads will be promoted on our Facebook page.</p>
+                  <h3><span class="advert-red">x(7)</span> Digital Medias&nbsp;</h3>
+                  <p>Your ads promoted on ALL our Medias & Apps GC consumers like, love & follow.</p>
                </div>
             </div>
          </div>
          <!-- end service item -->
 
-          <!-- Start info item -->
-         <div class="col-md-4 wow fadeInLeft" data-wow-duration="500ms" data-wow-delay="500ms">
-            <div class="service-item">
-               <div class="service-icon">
-                  <i class="fas fa-palette fa-2x"></i>
-               </div>
-               <div class="service-desc" >
-                  <h3>FREE Artwork</h3>
-                  <p class="wordwrap">Bonus free artwork on (x4) advertisements valued at <strong>$300</strong></p>
-               </div>
-            </div>
-         </div>
+
          <!-- end service item -->
 
          <!-- Pricing -->
-         <div class="col-md-12">
+         <div class="col-md-12" style="margin-top:80px;">
             <h2 class="text-center" class="pt40"> Pricing </h2>
+            <p style="font-size:16px;" class="text-center">Free artwork and formatting for ALL medias with all orders proceeded online, 
+               saving you <strong>($450 - $600)</strong> straight up! <br> Choose your preferred package/deal</p>
          </div>
        
          <div class="columns">
             <ul class="price">
-               <li class="header">Package 1 (2 ads)</li>
-               <li class="grey">$750 / year + GST</li>
-               <li>Pay in full and get an extra 3 months advertising <strong>FREE</strong> <br>Saving you an additional <strong>$206.25</strong></li>
+               <li class="header">Package 1 - 50,000 Printed Ads x2 Digital</li>
+               <li class="grey">2 easy payments of $350 + GST</li>
+               <li>Pay $350 deposit NOW to start advertising<br> and $350 balance in 45 days.</li>
+               <li><a href="{{route('contact')}}" class="button">Buy Now</a></li>
                <li class="greyli"> <strong>OR</strong></li>
-               <li>Pay via interest free payment</li>
-               <li class="grey">$350 deposit and $9.90 / week</li>
-               <li><a href="{{route('contact')}}" class="button">Contact Us to Buy Now</a></li>
+               <li>Pay in full and get an extra 3 months advertising <strong>FREE</strong> <br> Advertising you right through 2021,
+               saving you <strong>$206.25</strong></li>
+               <li class="grey">$750 / year + GST</li>
+               <li><a href="{{route('contact')}}" class="button">Buy Now</a></li>
             </ul>
          </div>
          <div class="columns">
             <ul class="price">
-               <li class="header">Package 2 (4 ads)</li>
-               <li class="grey">$1500 / year + GST</li>
-               <li>Pay in full and get an extra 3 months advertising <strong>FREE</strong> <br>Saving you an additional <strong>$412.50</strong></li>
+               <li class="header">Package 2 - 250,000 Printed Ads x3 Digital</li>
+               <li class="grey">3 easy payments of $500 + GST</li>
+               <li>Pay $750 deposit NOW to start advertising <br> and $750 balance in 30 days.</li>
+               <li><a href="{{route('contact')}}" class="button">Buy Now</a></li>
                <li class="greyli"> <strong>OR</strong></li>
-               <li>Pay via interest free payment</li>
-               <li class="grey">$750 deposit and $18.75 / week</li>
-               <li><a href="{{route('contact')}}" class="button">Contact us to Buy Now</a></li>
+               <li>Pay in full and get an extra 3 months advertising <strong>FREE</strong> <br> Advertising you right through 2021,
+                  saving you <strong>$412.50</strong></li>
+               <li class="grey">$1500 / year + GST</li>
+               <li><a href="{{route('contact')}}" class="button">Buy Now</a></li>
             </ul>
          </div>
       </div>
